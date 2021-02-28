51af7f826d2a1c4bc2aaf5bf7ffa95c484394f9a
Siana <sianush.sian@gmail.com>
2021-02-28 11:24:37 -0600

    primeras modificaciones

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,10 +12,10 @@ const Header = props => {
         <div className="header">
             <Link to="/">Home</Link>
             <div className="menu">
-                <Link to="/audio">Audio</Link>
-                <Link to="/iluminacion">Iluminacion</Link>
-                <Link to="/dj">Dj</Link>
-                <Link to="/video">Video</Link>
+                <Link to="/audio" className="haudio">Audio</Link>
+                <Link to="/iluminacion" className="hilumina">Iluminacion</Link>
+                <Link to="/dj" className="hdj">Dj</Link>
+                <Link to="/video" className="hvideo">Video</Link>
             </div>
             <div className="cart">
                 <Link to="/cotizacion">
